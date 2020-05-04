05c9b44dc1adb464bef8a3ca4dc5edd15f83a4a8
Akshant Jain <ajain78@uic.edu>
2020-05-04 19:59:49 +0000

    update

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ database.version(1).stores({
 if (navigator.onLine) {  
     console.log("You are online");
     database.open();
-    // fetch_data();
+//     fetch_data();
 }
 else {
     database.open();
@@ -23,25 +23,22 @@ else {
 function fetch_data() {
     console.log("Fetch Data");
 
-    fetch("https://covid19-data.p.rapidapi.com/india", 
-    {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "covid19-data.p.rapidapi.com",
-            "x-rapidapi-key": "57952ad950mshd7f574b2b26873fp1e0df2jsnb64a359d724a"
-        }
-    })
+    fetch("https://coronavirus-tracker-india-covid-19.p.rapidapi.com/api/getStatewise", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "coronavirus-tracker-india-covid-19.p.rapidapi.com",
+		"x-rapidapi-key": "57952ad950mshd7f574b2b26873fp1e0df2jsnb64a359d724a"
+	}
+})
     .then(response => response.json())
     .then(data => {
         database.india_states.clear();
         database.open().then(function () {
             for (let state of data) {
                 database.india_states.add({
-                    name: state.state,
-                    // latitude: state.latitude,
-                    // longitude: state.longitude,
-                    confirmed: state.confirmed,
-                    active: state.active, 
+                    name: state.name,
+                    confirmed: state.cases,
+//                     active: state.active, 
                     deaths: state.deaths,
                     recovered: state.recovered
                 });
@@ -67,8 +64,6 @@ function fetch_data() {
         for (let state of data) {
             database.usa_states.add({
                 name: state.state,
-                // latitude: state.latitude,
-                // longitude: state.longitude,
                 confirmed: state.confirmed,
                 deaths: state.deaths,
             })
@@ -78,32 +73,32 @@ function fetch_data() {
         console.log(err);
     });
 
-    fetch("https://covid19-data.p.rapidapi.com/all", 
-    {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "covid19-data.p.rapidapi.com",
-            "x-rapidapi-key": "83f25966e1msh579d26b19484f37p1594dejsn5e57a53531ec"
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        // console.log(data);
-        database.world.clear();
-        for (let country of data)   {
-            database.world.add({
-                name: country.country,
-                // latitude: country.latitude, 
-                // longitude: country.longitude, 
-                confirmed: country.confirmed, 
-                deaths: country.deaths, 
-                recovered: country.recovered
-            })
-        }
-    })
-    .catch(err => {
-        console.log(err);
-    });  
+//     fetch("https://covid19-data.p.rapidapi.com/all", 
+//     {
+//         "method": "GET",
+//         "headers": {
+//             "x-rapidapi-host": "covid19-data.p.rapidapi.com",
+//             "x-rapidapi-key": "83f25966e1msh579d26b19484f37p1594dejsn5e57a53531ec"
+//         }
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         // console.log(data);
+//         database.world.clear();
+//         for (let country of data)   {
+//             database.world.add({
+//                 name: country.country,
+//                 // latitude: country.latitude, 
+//                 // longitude: country.longitude, 
+//                 confirmed: country.confirmed, 
+//                 deaths: country.deaths, 
+//                 recovered: country.recovered
+//             })
+//         }
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });  
 };
 
 google.charts.load('current', {'packages': ['bar']});
@@ -168,6 +163,10 @@ document.querySelector(".search_screen .description").innerHTML = info[1].search
 let india_list;
 database.india_states.toArray().then((arr) => {
     india_list = arr;
+    
+    if (india_list.length == 0) {
+        fetch_data();
+    }
 });
 
 let usa_list;
@@ -359,7 +358,7 @@ function table_india() {
         // console.log(arr[0]);
         document.getElementById("india_state_name").innerHTML = arr[0].name;
         document.getElementById("india_confirmed").innerHTML = arr[0].confirmed;
-        document.getElementById("india_active").innerHTML = arr[0].active;
+//         document.getElementById("india_active").innerHTML = arr[0].active;
         document.getElementById("india_recovered").innerHTML = arr[0].recovered;
         document.getElementById("india_deaths").innerHTML = arr[0].deaths;
     })
@@ -415,7 +414,7 @@ function drawChart() {
 
     if (data_type == "india")    {
         data.addColumn('number', 'Confirmed');
-        data.addColumn('number', 'Active');
+//         data.addColumn('number', 'Active');
         data.addColumn('number', 'Deaths');
         data.addColumn('number', 'Recovered');
 
@@ -427,7 +426,7 @@ function drawChart() {
             let rowData = [];
             rowData.push(row.name);
             rowData.push(row.confirmed);
-            rowData.push(row.active);
+//             rowData.push(row.active);
             rowData.push(row.deaths);
             rowData.push(row.recovered);
             wholeRowData.push(rowData);
